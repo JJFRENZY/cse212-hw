42f728e73f634866aff5376a8d2c90142458c4eb
JJFRENZY <jackjayeck@icloud.com>
2025-07-04 17:23:32 -0600

    week01

--- week01/analyze/Sorting.cs
+++ week01/analyze/Sorting.cs
@@ -1,17 +1,38 @@
-﻿public static class Sorting {
-    public static void Run() {
-        var numbers = new[] { 3, 2, 1, 6, 4, 9, 8 };
-        SortArray(numbers);
-        Console.Out.WriteLine("int[]{{{0}}}", string.Join(", ", numbers)); //int[]{1, 2, 3, 4, 6, 8, 9}
-    }
+﻿using System;
 
-    private static void SortArray(int[] data) {
-        for (var sortPos = data.Length - 1; sortPos >= 0; sortPos--) {
-            for (var swapPos = 0; swapPos < sortPos; ++swapPos) {
-                if (data[swapPos] > data[swapPos + 1]) {
-                    (data[swapPos + 1], data[swapPos]) = (data[swapPos], data[swapPos + 1]);
+namespace Week01
+{
+    public static class Sorting
+    {
+        /// <summary>
+        /// Sorts an integer array using the Bubble Sort algorithm.
+        /// </summary>
+        /// <param name="data">The array to sort</param>
+        public static void SortArray(int[] data)
+        {
+            for (var sortPos = data.Length - 1; sortPos >= 0; sortPos--)
+            {
+                for (var swapPos = 0; swapPos < sortPos; ++swapPos)
+                {
+                    if (data[swapPos] > data[swapPos + 1])
+                    {
+                        (data[swapPos + 1], data[swapPos]) = (data[swapPos], data[swapPos + 1]);
+                    }
                 }
             }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new[] { 3, 2, 1, 6, 4, 9, 8 };
+
+            Sorting.SortArray(numbers);
+
+            Console.WriteLine("int[]{{{0}}}", string.Join(", ", numbers));
+            // Expected output: int[]{1, 2, 3, 4, 6, 8, 9}
         }
     }
 }
