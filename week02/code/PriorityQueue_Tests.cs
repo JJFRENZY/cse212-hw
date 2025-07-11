b7380ea6c82083c65e3a626eba2a9ca34eaaba0a
JJFRENZY <jackjayeck@icloud.com>
2025-07-11 01:15:59 -0600

    Week03

--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -1,29 +1,48 @@
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-// TODO Problem 2 - Write and run test cases and fix the code to match requirements.
-
 [TestClass]
-public class PriorityQueueTests
+public class PriorityQueue_Tests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_1()
+    public void TestEnqueueDequeue()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+        var pq = new PriorityQueue();
+
+        pq.Enqueue("LowPriority", 1);
+        pq.Enqueue("MediumPriority", 5);
+        pq.Enqueue("HighPriority", 10);
+        pq.Enqueue("SameHighPriority", 10);
+
+        // Dequeue should return item with highest priority (last added with highest priority)
+        var first = pq.Dequeue();
+        Assert.AreEqual("SameHighPriority", first);
+
+        // Next highest priority is HighPriority (priority 10)
+        var second = pq.Dequeue();
+        Assert.AreEqual("HighPriority", second);
+
+        // Next is MediumPriority (priority 5)
+        var third = pq.Dequeue();
+        Assert.AreEqual("MediumPriority", third);
+
+        // Last is LowPriority (priority 1)
+        var fourth = pq.Dequeue();
+        Assert.AreEqual("LowPriority", fourth);
+
+        // Queue is empty now, next Dequeue throws
+        Assert.ThrowsException<InvalidOperationException>(() => pq.Dequeue());
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
-    public void TestPriorityQueue_2()
+    public void TestToString()
     {
-        var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
-    }
+        var pq = new PriorityQueue();
+        pq.Enqueue("A", 3);
+        pq.Enqueue("B", 2);
 
-    // Add more test cases as needed below.
+        var str = pq.ToString();
+        StringAssert.Contains(str, "A (Pri:3)");
+        StringAssert.Contains(str, "B (Pri:2)");
+    }
 }
